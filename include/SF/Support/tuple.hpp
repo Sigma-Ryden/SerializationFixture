b83f092105bf63eb8df0a4ef9a1f5d7c14f8ab6b
Sigma-Ryden <71389444+Sigma-Ryden@users.noreply.github.com>
2023-04-22 14:33:45 +0300

    Update tuple.hpp

--- include/SF/Support/tuple.hpp
+++ include/SF/Support/tuple.hpp
@@ -24,8 +24,7 @@ namespace detail
 {
 
 template <class Archive, class T, std::size_t... I,
-          SFREQUIRE(meta::is_archive<Archive>() and
-                    meta::is_std_tuple<T>::value)>
+          SFREQUIRE(meta::is_std_tuple<T>::value)>
 void expand(Archive& archive, T& tuple, meta::index_sequence<I...>)
 {
     archive(std::get<I>(tuple)...);
